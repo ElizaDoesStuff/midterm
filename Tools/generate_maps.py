3facc03f6decd8c7ba1583b299983e86068f5b23
ElizaDoesStuff <eliza.doesstuff00100@gmail.com>
2025-08-02 14:46:22 -0500

    Add tools for generating maps.

--- Tools/generate_maps.py
+++ Tools/generate_maps.py
@@ -1,5 +1,16 @@
-with open("Keys.enum") as file:
-	file = file.readlines()
-	for line in file.tostring("").split(", "):
-		line = line.split(", ")
-	print(file.join("\n"))
+def gen_map(name,values1,values2,type1,type2,prefix1,prefix2):
+	if not type(values1) is list: print(type(values1)); print("Ërror: You cannot map one value to multiple values."); exit(1)
+	print("std::unordered_map<" + type1 + "," + type2 + "> " + name + " {")
+	out = ""
+	for i in range(len(values1)):
+		if values1[i] == "": out += "\n\t"; continue
+		out += ("{" + prefix1 + values1[i] + ", " + prefix2 + (values2[i] if isinstance(values2,list) else values2) + "}, ")
+	print(out)
+	print("};")
+
+keysfile = open("./Keys.enum")
+keys = keysfile.readlines()
+for i in range(len(keys)):
+	keys[i] = keys[i].strip()
+gen_map("key_states",keys,"false","Keys","bool","Keys::","")
+keysfile.close()
