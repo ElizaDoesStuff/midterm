31bf7000159d6940bd7ada889ad81ac2e9b1396b
ElizaDoesStuff <eliza.doesstuff00100@gmail.com>
2025-08-03 10:12:47 -0500

    Update .gitignore to not add my custom batch script for opening the project workspace

--- Tests/KeyInput/main.cpp
+++ Tests/KeyInput/main.cpp
@@ -5,10 +5,14 @@ bool shouldExit = false;
 
 void onkey(KeyActions action, Keys key, KeyFlags flags) {
 	if (action == KeyActions::REPEAT) return;
-	if ((key == Keys::ESC) && action == KeyActions::PRESS) shouldExit = true;
+	shouldExit = (action == KeyActions::PRESS) && ( (key == Keys::ESC) || (key == Keys::C && (flags & 0b11000000 > 0)) );
 
-	std::cout << static_cast<unsigned int>(key) << "\n";
-	std::cout << (action == KeyActions::PRESS ? "Key was pressed." : "Key was released.") << "\n";
+	std::cout << "Key " + std::to_string( static_cast<int>(key) ) + (action == KeyActions::PRESS ? " was pressed." : " was released.") << std::endl;
+	std::cout << "Ctrl key was " + std::string((flags & 0b10000000) != 0 ? "down." : "up.") << std::endl;
+	std::cout << "Alt key was " + std::string((flags & 0b01000000) != 0 ? "down." : "up.") << std::endl;
+	std::cout << "Shift key was " + std::string((flags & 0b00100000) != 0 ? "down." : "up.") << std::endl;
+	std::cout << "Caps Lock was " + std::string((flags & 0b00010000) != 0 ? "on." : "off.") << std::endl;
+	std::cout << "\n";
 };
 
 int main() {
